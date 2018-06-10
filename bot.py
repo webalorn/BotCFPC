6f5316cf6e6ce3947b9de78135caa09ece63062b
Hugo <hugo.pm@protonmail.com>
2018-06-10 16:38:33 +0200

    Unification en un fichier

--- bot.py
+++ bot.py
@@ -3,3 +3,9 @@ from discord.ext import commands
 
 prefix = "&"
 bot = commands.Bot(command_prefix=prefix)
+
+token = ""
+with open("token", "r") as f:
+    token = f.read()
+
+bot.run(token)
--- main.py
+++
@@ -1,8 +0,0 @@
-from bot import bot
-import misc
-
-token = ""
-with open("token", "r") as f:
-    token = f.read()
-
-bot.run(token)
